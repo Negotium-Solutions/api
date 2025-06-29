ef72a4d5762f663242cb975737d689686898db84
Klaas <kmrikhotso@gmail.com>
2025-06-29 22:27:10 +0200

    Install base - Install sanctum - Install

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,18 @@ use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByPath;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 use App\Http\Controllers\Api\Central\Auth\AuthController;
+use App\Http\Controllers\Api\Central\TenantController;
 
-Route::get('/ping', function () {
-    return ['message' => 'pong'];
-});
-
+// Central routes - Start
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
+// Central routes - End
 
 Route::middleware([InitializeTenancyByPath::class, PreventAccessFromCentralDomains::class, 'auth:sanctum'])->group(function () {
-    //
+    Route::prefix('tenant')->group(function () {
+        Route::get('/create', [TenantController::class, 'create'])->name('tenant.create');
+    });
 });
